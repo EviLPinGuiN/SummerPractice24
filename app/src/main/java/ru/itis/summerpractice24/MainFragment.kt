e58764ed87a0b2e05dcb8d79fd655b025e447ccd
Nail <shaykhrazievnail@gmail.com>
2024-07-02 12:53:26 +0300

    Add example with shared preferences

--- app/src/main/java/ru/itis/summerpractice24/MainFragment.kt
+++ app/src/main/java/ru/itis/summerpractice24/MainFragment.kt
@@ -1,10 +1,13 @@
 package ru.itis.summerpractice24
 
+import android.content.Context
 import android.os.Bundle
 import android.view.View
+import androidx.core.content.edit
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import ru.itis.summerpractice24.databinding.FragmentMainBinding
+import ru.itis.summerpractice24.utils.showSnackbar
 
 class MainFragment : Fragment(R.layout.fragment_main) {
 
@@ -14,10 +17,23 @@ class MainFragment : Fragment(R.layout.fragment_main) {
         super.onViewCreated(view, savedInstanceState)
         binding = FragmentMainBinding.bind(view)
 
+        val pref = context?.getSharedPreferences("Default", Context.MODE_PRIVATE)
+
         binding?.run {
+
+            pref?.getString("ARG_TEXT", "")
+                ?.takeIf { it.isNotEmpty() }
+                ?.let {
+                    root.showSnackbar(it)
+                }
+
             tvTitle.setOnClickListener {
 
-//                val text = etEmail.text.toString()
+                val text = etEmail.text.toString()
+
+                pref?.edit {
+                    putString("ARG_TEXT", text)
+                }
 //                val bundle = Bundle()
 //                bundle.putString("ARG_EMAIL", text)
 
@@ -25,7 +41,7 @@ class MainFragment : Fragment(R.layout.fragment_main) {
                 findNavController().navigate(
                     resId = R.id.action_mainFragment_to_profileFragment,
                     args = ProfileFragment.bundle(
-                        email = etEmail.text.toString(),
+                        email = text,
                         age = 10,
                     )
                 )
